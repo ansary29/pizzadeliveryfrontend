ba9cfa45452110ef7f69b7568090d1799fe0d3e7
ansary29 <85570638+ansary29@users.noreply.github.com>
2022-03-18 19:49:35 +0530

    main

---
+++ src/screens/Home.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Pizza from "../Components/Pizza";
+import Souce from "../Components/Souce";
+import "../App.css";
+import axios from "axios";
+import {useState,useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { getAllPizzas } from "../Actions/PizzaActions";
+import Error from "../Components/Error";
+import Loading from "../Components/Loading";
+
+
+export default function Home(){
+    // get pizza actions //
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getAllPizzas(pizzas))
+        //eslint-disable-next-line
+    },[]);
+
+    //  get pizza data to home screen //
+    //eslint-disable-next-line
+    const [pizzas,setPizzaList] = useState([])
+    //eslint-disable-next-line
+    useEffect(async () => {
+        const response = await axios.get('https://pizzadeliveryback.herokuapp.com/Pizza/getpizza');
+        setPizzaList(response.data);
+    },[])
+
+    const pizzasstate = useSelector((state) => state.getAllPizzasReducer);
+
+    const {error, loading } = pizzasstate;
+
+    return(
+
+        // it is run by pizzasdata
+       <>
+       {/* render the full data */}
+       {loading ? (
+          <Loading/>
+        ) : error ? (
+          <Error error='Something went wrong'/>
+        ) : 
+        (<>
+        <div className="row">
+            {pizzas.map(pizza=>{
+                return <div className="col-md-4" key={Pizza._id}>
+                    <div>
+                        <Pizza pizzas={pizza}/>  
+                    </div>
+                </div>
+            })}
+        </div>
+         <Soucees/>
+         </>)}
+      </>
+    )
+}
+
+// it is run by soucesdata
+ function Soucees(){
+
+    //  get souce data to home page //
+    const [souces,setSouceList] = useState([])
+    //eslint-disable-next-line
+    useEffect(async () => {
+        const response = await axios.get('https://pizzadeliveryback.herokuapp.com/Souce/getsouce');
+        setSouceList(response.data);
+    },[])
+
+    return(
+        <div className="row">
+            {souces.map(souce=>(
+                 <div className="col-md-4" keys={Souce._id}>
+                    <div>
+                        <Souce souces={souce} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
---
+++ src/screens/Soucelist.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from '@mui/material/Button';
+
+export default function Soucelist() {
+    
+    const [souces,setSouceList] = useState([])
+    //eslint-disable-next-line
+      useEffect(async () => {
+        const response = await axios.get('https://pizzadeliveryback.herokuapp.com/Souce/getsouce');
+        setSouceList(response.data);
+    },[])
+
+    const deleteSouce = async(id) =>{
+        var response = await axios.delete(`https://pizzadeliveryback.herokuapp.com/Souce/deletesouce/${id}`, {souces})
+        console.log(response);
+        window.location.reload()
+    }
+
+    return (
+        <div>
+         <h3>Soucelist</h3>
+        <table  className='table table-bordered border-primary table-responsive-sm'>
+    
+            <thead className='table-dark'>
+                <tr>
+                    <th>Name</th>
+                    <th>Prices</th>
+                    <th>Category</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            {souces && souces.map(souce=>{
+    
+                return <tr>
+                    <td>{souce.name}</td>
+                    <td>
+                      {souce.price}
+                    </td>
+                    <td>{souce.category}</td>
+                    <td>
+                    <Button variant="contained" color="error" style={{marginRight:"10px"}} onClick={()=>deleteSouce(souce._id)}>Delete</Button>
+                    </td>
+                </tr>
+            })}
+            </tbody>
+    
+        </table>
+            </div>
+    )
+}
